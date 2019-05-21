1c8b719a861488091ff50986a89b794be8fdeecb
Sindre Sorhus <sindresorhus@gmail.com>
2019-05-21 14:24:58 +0700

    Meta tweaks

--- fixture-multi-line.js
+++ fixture-multi-line.js
@@ -5,10 +5,8 @@ const {app, BrowserWindow} = require('electron');
 	await app.whenReady();
 
 	const win = new BrowserWindow({show: true});
-	win.loadURL('https://google.com');
+	await win.loadURL('https://google.com');
 	win.webContents.openDevTools();
-	win.webContents.on('dom-ready', () => {
-		console.log('\n\n🦄\n\n\n🦄\n\n🦄\n\n');
-		setTimeout(app.quit, 3000);
-	});
+	console.log('\n\n🦄\n\n\n🦄\n\n🦄\n\n');
+	setTimeout(app.quit, 3000);
 })();
